ba326a46a1a942a62ffe77d5e632fec358e93288
zaynzareer <73751728+zaynzareer@users.noreply.github.com>
2024-12-16 08:45:46 +0530

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,73 @@
+const checkoutitem = document.getElementById("checkoutbox");
+const showTotalQnty = document.getElementById("showTotalQnty");
+const showTotalPrice = document.getElementById("showTotalPrice");
+
+function checkout(){
+    //checkoutitem.innerText = "HI this is the new page";
+    let checkoutList = JSON.parse(localStorage.getItem("item"));
+    //let subtotal = 0;
+    let grandTotal = 0;
+    let totalQuantity = 0;
+    if(checkoutList){
+        checkoutList.forEach((item) => {
+            if(item){
+            let checkoutitems = document.createElement("div");
+            checkoutitems.classList.add("checkoutlist");
+            checkoutitems.innerHTML = `
+            <div>${item.name}</div>
+            <div>Rs. ${item.price}</div>
+            <div>${item.quantity}</div>
+            <div>Rs. ${item.price * item.quantity}</div>
+            `; 
+            checkoutitem.append(checkoutitems);
+            grandTotal = grandTotal + (item.price * item.quantity)
+            totalQuantity = totalQuantity + item.quantity;
+            }
+        });
+    };
+    showTotalQnty.innerText = totalQuantity;
+    showTotalPrice.innerText = "Rs. " + grandTotal;
+}
+checkout();
+
+//toggle card details on and off
+
+const radioOptions = document.querySelectorAll("input[name='payment']");
+const cardDetails = document.querySelectorAll(".carddetails");
+
+radioOptions.forEach(radio => {
+  radio.addEventListener("change", showCardDetails);
+});
+
+function showCardDetails() {
+  if (this.value === "card") {
+    cardDetails.forEach(div => {
+      div.style.display = "block";
+    });
+  } else {
+    cardDetails.forEach(div => {
+      div.style.display = "none";
+    });
+  }
+}
+
+
+const payButton = document.getElementById("paybtn");
+const popup = document.getElementById("popup")
+const opacityitem = document.querySelector("#checkoutsection");
+payButton.addEventListener("click",displayMessage);
+function displayMessage(){
+  popup.style.top = "30%";
+  popup.style.transform = "scale(1)";
+  popup.style.display = "block";
+  opacityitem.style.opacity = "0.4";
+}
+
+//date function
+const datetext = document.getElementById("orderDate")
+function calDate(){
+  date = new Date();
+  date.setDate(date.getDate() + 10);
+  datetext.innerText = date.toDateString();
+}
+calDate()
---
+++ main.js
@@ -0,0 +1,154 @@
+const itembox = document.getElementById("itembox");
+const tableEntry = document.getElementById("carttable");
+const total = document.getElementById("total");
+const buybtn = document.getElementById("buynow")
+let items;
+let tableitems = [];
+ 
+window.addEventListener("load", loaditems);
+
+function loaditems(){
+    fetch("medicines.json")
+    .then(item => item.json())
+    .then(data => processitem(data))
+    .catch(error => console.error(`Error occurred: ${error}`))
+}
+
+buybtn.disabled = true;
+const processitem = (data) => {
+    localStorage.removeItem("item");
+    items = data;
+    const categories = ["Antibiotics", "Analgesics", "Antidepressants", "Antihistamines", "Antihypertensives"];
+    categories.forEach(category => {
+        const catsection = document.createElement("section");
+        catsection.classList.add("categorySection");
+
+        const catheader = document.createElement("h2");
+        catheader.innerText = category;
+        catsection.append(catheader);
+        const itemSection = document.createElement("div");
+        itemSection.classList.add("itemSection");
+
+        items.filter(item => item.category == category).forEach((medicine) => {
+            let itemcards = document.createElement("div");
+            itemcards.classList.add("itemcard");
+            itemcards.setAttribute("data-id", medicine.id);
+            itemcards.innerHTML = `
+            <img src=${medicine.image} alt="medicine image">
+            <div class="title">${medicine.name}</div>
+            <div class="price">Rs. ${medicine.price}</div>
+            <div class="quantitydiv">
+                <button onclick="changeQuantity(${medicine.id}, '-')"> - </button>
+                <span class="quantity"> 0 </span>
+                <button onclick="changeQuantity(${medicine.id}, '+')"> + </button> 
+            </div>
+            `;
+            itemSection.append(itemcards);
+            catsection.append(itemSection);
+        });
+        itembox.append(catsection);
+    });
+}
+
+function changeQuantity(medicineid, type){
+    const itemdiv = document.querySelector(`.itemcard[data-id = "${medicineid}"]`);
+    const qnty = itemdiv.querySelector(".quantity");
+    switch(type){
+        case '+':
+            copyofitems = JSON.parse(JSON.stringify(items));
+            if(!tableitems[medicineid]){
+                tableitems[medicineid] = copyofitems.filter(item => item.id == medicineid)[0];
+                tableitems[medicineid].quantity = 1;
+                qnty.innerText = 1;
+            }else{
+                tableitems[medicineid].quantity++;
+                qnty.innerText = tableitems[medicineid].quantity;
+            }
+            break;
+        case '-':
+            tableitems[medicineid].quantity--;
+            qnty.innerText = tableitems[medicineid].quantity;
+            if(tableitems[medicineid].quantity <= 0){
+                delete tableitems[medicineid];
+                qnty.innerText = 0;
+                break;
+            }
+        default:
+            break;
+    }
+    reloadTable();
+    buybtn.disabled = false;
+}
+
+function reloadTable(){
+    tableEntry.innerHTML= '';
+    let totalprice = 0;
+    tableitems.forEach((medicine) => {
+        //totalprice = totalprice + medicine.price;
+        if(medicine != null){
+            let newentry = document.createElement("tr");
+            let itemtotalprice = medicine.price * medicine.quantity;
+            totalprice += itemtotalprice;
+            newentry.innerHTML = `
+            <td>${medicine.name}</td>
+            <td>${medicine.quantity}</td>
+            <td>Rs. ${medicine.price}</td>`;
+            tableEntry.append(newentry);
+        }
+    })
+    total.innerText = "Rs. " + totalprice;
+    //console.log("tableitems: ", tableitems);
+    addToLocalStorage();
+}
+
+function addToLocalStorage(){
+    localStorage.setItem("item", JSON.stringify(tableitems));
+}
+
+
+const addToTavouritesbtn = document.getElementById("addToFavourites");
+const applyFavouritesbtn = document.getElementById("applyFavourites");
+
+//testing
+
+addToTavouritesbtn.addEventListener("click", addToFavourites);
+applyFavouritesbtn.addEventListener("click", applyFavourites);
+
+
+function addToFavourites(){
+    if(tableitems === null){
+        window.alert("No Items added to Cart!");
+    } else{
+        let checkoutlist =  JSON.stringify(tableitems);
+        localStorage.setItem("favouriteItem", checkoutlist);
+        //console.log("added to favourites: ", checkoutlist);
+        window.alert("Items have been added to local Storage!");
+    }
+}
+
+function applyFavourites(){
+    //checkoutliststrings = JSON.parse(checkoutlist);
+    //checkoutitem.innerText = checkoutliststrings;
+    let checkoutliststuff = JSON.parse(localStorage.getItem("favouriteItem"));
+    tableitems = checkoutliststuff;
+
+    tableEntry.innerHTML= '';
+    let totalprice = 0;
+    tableitems.forEach((stuff) => {
+        //totalprice = totalprice + stuff.price;
+        if(stuff != null){
+            let newentry = document.createElement("tr");
+            let itemtotalprice = stuff.price * stuff.quantity;
+            totalprice += itemtotalprice;
+            newentry.innerHTML = ` 
+            <td>${stuff.name}</td>
+            <td>${stuff.quantity}</td>
+            <td>Rs. ${stuff.price}</td>`;
+            tableEntry.append(newentry);
+        }
+    })
+    total.innerText = "Rs. " + totalprice;
+    //console.log("checkoutliststuff: ", checkoutliststuff);
+    //console.log("tableitesm: ", tableitems);
+    addToLocalStorage();
+}
